9bd10c1de81fee0596cb9b7d6d66bea2c13c56b3
Micro Neko <microneko@My-Mac-mini.local>
2024-12-02 11:58:42 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +9,31 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  private formBuilder = inject(FormBuilder);
+
   title = 'bmi-calculator';
+
+  calculatorForm = this.formBuilder.group({
+    weight: [''],
+    height: [''],
+  });
+
+  calculateBmi = () => {
+    let height = 0;
+    let weight = 0;
+
+    if (this.calculatorForm.value.height) {
+      height = parseFloat(this.calculatorForm.value.height);
+    }
+
+    if (this.calculatorForm.value.weight) {
+      weight = parseFloat(this.calculatorForm.value.weight);
+    }
+
+    if (height === 0) {
+      return 0;
+    }
+
+    return weight / ((height / 100) ^ 2);
+  }
 }
